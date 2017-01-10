6ab213e7581761710ae781b0ee8c5c7ad55f2ed9
Tim Swalling <tim@timswalling.com>
2017-01-10 09:56:53 +0000

    Add basic request parrot server and tests

--- server.js
+++ server.js
@@ -1,1 +1,53 @@
 'use strict';
+
+const http = require('http');
+const querystring = require('querystring');
+const url = require('url');
+
+const port = process.env.PORT || 3000;
+
+
+const listen = function (err) {
+  if (err) {
+    console.error(err);
+    return false;
+  }
+
+  console.info(`Server is listening on ${port}`);
+};
+
+
+const parrot = function (request, response) {
+  let responseHeaders = {
+    'Cache-Control': 'private',
+    'Content-Type': 'application/json'
+  };
+  let requestBody = '';
+  let requestData = {
+    headers: request.headers
+  };
+  let requestUrl = `${request.protocol || request.headers['x-forwarded-proto'] || 'http'}://${request.headers.host}${request.url}`;
+
+  requestData.url = url.parse(requestUrl);
+
+  request.on('data', function (chunk) {
+    requestBody += chunk.toString();
+  });
+
+  request.on('end', function () {
+    if (requestBody) {
+      requestData.body = querystring.parse(requestBody);
+    }
+
+    let responseBody = JSON.stringify(requestData, null, 4);
+
+    response.writeHead(200, responseHeaders);
+    response.write(responseBody);
+    response.end();
+  });
+};
+
+
+const server = http.createServer(parrot);
+
+server.listen(port, listen);
---
+++ test/server.js
@@ -0,0 +1,69 @@
+/* eslint-env mocha */
+'use strict';
+
+const assert = require('assert');
+const request = require('request');
+
+const port = process.env.PORT || 3000;
+
+require('../server');
+
+
+describe('Request Parrot', function () {
+  it('should return JSON data containing the request headers', function (done) {
+    let customHeaders = {
+      'User-Agent': 'mocha',
+      'X-Foo': 'bar'
+    };
+    let options = {
+      headers: customHeaders,
+      json: true,
+      url: `http://localhost:${port}`
+    };
+
+    request(options, function (error, response, body) {
+      assert(!error && response.statusCode == 200 && 'headers' in body);
+
+      let hasAllHeaders = Object.keys(customHeaders).every(function (header) {
+        return header.toLowerCase() in body.headers;
+      });
+
+      assert(hasAllHeaders);
+      done();
+    });
+  });
+
+  it('should return JSON data containing the request\'s `application/x-www-form-urlencoded` data', function (done) {
+    let options = {
+      form: {
+        foo: 'bar'
+      },
+      json: true,
+      method: 'post',
+      url: `http://localhost:${port}`
+    };
+
+    request(options, function (error, response, body) {
+      assert(!error && response.statusCode == 200 && 'body' in body);
+
+      assert.deepEqual(options.form, body.body);
+
+      done();
+    });
+  });
+
+  it('should return JSON data containing the requested URL', function (done) {
+    let options = {
+      json: true,
+      url: `http://localhost:${port}/foo`
+    };
+
+    request(options, function (error, response, body) {
+      assert(!error && response.statusCode == 200 && 'url' in body);
+
+      assert.equal(options.url, body.url.href);
+
+      done();
+    });
+  });
+});
